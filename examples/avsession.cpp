ceede11e5938f5991e51775436a78be352a8498f
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2016-12-22 17:11:08 +0100

    The avsession example now uses the Qt5 output

--- examples/avsession.cpp
+++ examples/avsession.cpp
@@ -4,15 +4,23 @@
 
 #include <mipconfig.h>
 
-#if defined(MIPCONFIG_SUPPORT_QT) && defined(MIPCONFIG_SUPPORT_AVCODEC) \
+#if defined(MIPCONFIG_SUPPORT_QT5) && defined(MIPCONFIG_SUPPORT_AVCODEC) \
 	&& ( (defined(MIPCONFIG_SUPPORT_OSS) && defined(MIPCONFIG_SUPPORT_VIDEO4LINUX)) || \
 	     (defined(MIPCONFIG_SUPPORT_WINMM) && defined(MIPCONFIG_SUPPORT_DIRECTSHOW)) )
 
 #include <mipvideosession.h>
 #include <mipavcodecencoder.h>
 #include <mipaudiosession.h>
+#include <mipqt5outputwidget.h>
+#include <mippainputoutput.h>
 #include <iostream>
 #include <jrtplib3/rtpipv4address.h>
+#include <QtGui/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <stdlib.h>
+#ifndef WIN32
+#include <unistd.h>
+#endif // WIN32
 
 using namespace jrtplib;
 
@@ -21,7 +29,7 @@ void checkRet(bool ret,const MIPErrorBase &obj)
 	if (!ret)
 	{
 		std::cerr << obj.getErrorString() << std::endl;
-		exit(-1);
+		_exit(-1);
 	}
 }
 
@@ -71,8 +79,9 @@ protected:
 	}	
 };
 
-int main(void)
+int main(int argc, char *argv[])
 {
+	QApplication app(argc, argv);
 #ifdef WIN32
 	WSADATA dat;
 	WSAStartup(MAKEWORD(2,2),&dat);
@@ -85,6 +94,15 @@ int main(void)
 		return -1;
 	}
 #endif // WIN32
+#ifdef MIPCONFIG_SUPPORT_PORTAUDIO
+	std::string errStr;
+
+	if (!MIPPAInputOutput::initializePortAudio(errStr))
+	{
+		std::cerr << "Can't initialize PortAudio: " << errStr << std::endl;
+		return -1;
+	}
+#endif // MIPCONFIG_SUPPORT_PORTAUDIO
 	
 	MIPAVCodecEncoder::initAVCodec();
 	
@@ -95,7 +113,7 @@ int main(void)
 	bool ret;
 
 	std::string bandWidth;
-	std::cout << "Enter bandwidth" << std::endl;
+	std::cout << "Enter bandwidth for video (in bits per second, e.g. 200000)" << std::endl;
 	std::cin >> bandWidth;
 
 	int bw = atoi(bandWidth.c_str());
@@ -124,6 +142,25 @@ int main(void)
 	
 	std::cout << "Starting audio session at portbase " << audioPort << ", video session at portbase " << videoPort << std::endl;
 
+	std::string destStr;
+	std::cout << "Enter destination IP, audio portbase and video portbase (format: a.b.c.d:AudioPort:VideoPort)" << std::endl;
+	std::cout << "(Note that video is automatically sent to yourself in this example)" << std::endl;
+	std::cin >> destStr;
+
+	int ip[4] = { 127, 0, 0, 1};
+	int Aport = 6000;
+	int Vport = 6100;
+	int status = sscanf(destStr.c_str(), "%d.%d.%d.%d:%d:%d", &ip[0], &ip[1], &ip[2], &ip[3], &Aport, &Vport);
+	if (status != 6)
+	{
+		std::cerr << "Bad destination" << std::endl;
+		return -1;
+	}
+
+	// We'll need to create the GUI very shortly after initializing the
+	// session, to make sure that we're not missing the Qt signal that a
+	// new source was added
+
 	ret = videoSess.init(&Vparams);
 	checkRet(ret, videoSess);
 
@@ -134,20 +171,6 @@ int main(void)
 	ret = videoSess.addDestination(RTPIPv4Address(ipLocal, videoPort));
 //	ret = audioSess.addDestination(RTPIPv4Address(ipLocal, audioPort));
 
-	int ip[4] = { 127, 0, 0, 1};
-	int Aport = 6000;
-	int Vport = 6100;
-	
-	std::string destStr;
-	std::cout << "Enter destination IP, audio portbase and video portbase (format: a.b.c.d:AudioPort:VideoPort)" << std::endl;
-	std::cin >> destStr;
-
-	int status = sscanf(destStr.c_str(), "%d.%d.%d.%d:%d:%d", &ip[0], &ip[1], &ip[2], &ip[3], &Aport, &Vport);
-	if (status != 6)
-	{
-		std::cerr << "Bad destination" << std::endl;
-	}
-	else
 	{
 		uint8_t ip2[4];
 
@@ -158,14 +181,27 @@ int main(void)
 		ret = videoSess.addDestination(RTPIPv4Address(ip2,Vport));
 		ret = audioSess.addDestination(RTPIPv4Address(ip2,Aport));
 	
-		std::string str;
-		std::cout << "Type something to exit" << std::endl;
-		std::cin >> str;
+		MIPQt5OutputComponent *pOutputComp = videoSess.getQt5OutputComponent();
+		if (pOutputComp == 0)
+		{
+			std::cerr << videoSess.getErrorString() << std::endl;
+		}
+		else
+		{
+			std::cout << "Starting test GUI, close main window to exit..." << std::endl;
+			MIPQt5OutputMDIWidget testWidget(pOutputComp);
+
+			testWidget.show();
+			app.exec();
+		}
 	}
 	
 	videoSess.destroy();
 	audioSess.destroy();
 
+#ifdef MIPCONFIG_SUPPORT_PORTAUDIO
+	MIPPAInputOutput::terminatePortAudio();
+#endif // MIPCONFIG_SUPPORT_PORTAUDIO
 #ifdef WIN32
 	CoUninitialize();	
 	WSACleanup();
